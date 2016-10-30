f51c51f2b4ca56c806549828f349f7c1a3940a2a
kenta-s <knt01222@gmail.com>
2016-10-30 22:25:41 +0900

    Separate hero-detail.component.ts

--- angular-tour-of-heroes/app/app.component.ts
+++ angular-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
@@ -10,14 +12,7 @@ import { Component } from '@angular/core';
                  <span class="badge">{{hero.id}}</span> {{hero.name}}
                </li>
              </ul>
-             <div *ngIf="selectedHero">
-               <h2>{{selectedHero.name}} details!</h2>
-               <div><label>id: </label>{{selectedHero.id}}</div>
-               <div>
-                 <label>name: </label>
-                 <input [(ngModel)]="selectedHero.name" placeholder="name">
-               </div>
-             </div>
+             <my-hero-detail [hero]="selectedHero"></my-hero-detail>
             `,
   styles: [`
   .selected {
@@ -76,10 +71,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-export class Hero {
-  id: number;
-  name: string;
 }
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
