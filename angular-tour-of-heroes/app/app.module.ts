878c92bf40ea23086d2de4a8bd5338376ed22da6
kenta-s <knt01222@gmail.com>
2016-11-03 10:27:12 +0900

    Create DashboardComponent

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroService } from './hero.service';
 import { RouterModule }   from '@angular/router';
 
@@ -15,13 +16,23 @@ import { RouterModule }   from '@angular/router';
       {
         path: 'heroes',
         component: HeroesComponent
-      }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
     ])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
