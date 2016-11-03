1f3e93a1b22c4e41adb29be152ef2222217ef193
kenta-s <knt01222@gmail.com>
2016-11-03 15:47:56 +0900

    :tada: Complete Routing section

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -6,31 +6,14 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroService } from './hero.service';
-import { RouterModule }   from '@angular/router';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -42,4 +25,3 @@ import { RouterModule }   from '@angular/router';
   providers: [HeroService]
 })
 export class AppModule { }
-
